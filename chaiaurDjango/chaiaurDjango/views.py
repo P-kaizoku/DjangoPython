afd63f6fa14dc5ad738c7f82de16bd1777564f9b
Pabitra <pabitram322@gmail.com>
2024-05-24 09:02:39 +0530

    templates added using render

--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Welcome to the Home page")
+    # return HttpResponse("Welcome to the Home page")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("Welcome to the about page")
